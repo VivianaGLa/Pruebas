a0e3ccbd02a9fcb6460e996d8e37e1610362277e
Viviana Garcia <113049655+VivianaGLa@users.noreply.github.com>
2022-09-14 16:59:12 -0500

    Delete src directory

--- src/main/java/com/minsait/junit/exception/DineroInsuficienteException.java
+++
@@ -1,7 +0,0 @@
-package com.minsait.junit.exception;
-
-public class DineroInsuficienteException extends  RuntimeException{
-    public DineroInsuficienteException(String msj){
-        super(msj);
-    }
-}
